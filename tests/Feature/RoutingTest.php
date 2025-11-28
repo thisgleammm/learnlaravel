9a788e5726b30459db3d5f5969bca6efc56756f2
thisgleammm <thisgleam@gmail.com>
2025-11-28 08:32:00 +0700

    Route Parameter

--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -31,4 +31,43 @@ class RoutingTest extends TestCase
         $this->get('/tidakada')
             ->assertSeeText("404 by thisgleam");
     }
+
+    public function testRouteParameter()
+    {
+        $this->get('/products/1')
+            ->assertSeeText("Product ID: 1");
+    }
+
+    public function testRouteParameterWithMultiple()
+    {
+        $this->get('/products/1/items/2')
+            ->assertSeeText("Product: 1 Item: 2");
+    }
+
+    public function testRouteParameterRegex()
+    {
+        $this->get('/categories/1')
+            ->assertSeeText("Category: 1");
+
+        $this->get('/categories/abc')
+            ->assertSeeText("404 by thisgleam");
+    }
+
+    public function testRouteParameterOpsional()
+    {
+        $this->get('/users/1')
+            ->assertSeeText("User: 1");
+
+        $this->get('/users/')
+            ->assertSeeText("User: 404");
+    }
+
+    public function testRouteConflict()
+    {
+        $this->get('/conflict/bahlil')
+            ->assertSeeText("Conflict: bahlil");
+
+        $this->get('/conflict/gleam')
+            ->assertSeeText("Conflict: gleam");
+    }
 }
