3dfca6c0ea9b2905a0561f14c0b6e0c89f2303f6
thisgleammm <thisgleam@gmail.com>
2026-01-09 23:09:15 +0700

    HTTP Exception

--- routes/web.php
+++ routes/web.php
@@ -160,3 +160,13 @@ Route::get('/error/manual', function () {
 Route::get('/error/validation', function () {
     throw new ValidationException("Validation Error");
 });
+
+Route::get('/abort/400', function () {
+    abort(400, 'Ups Validation Error');
+});
+Route::get('/abort/401', function () {
+    abort(401);
+});
+Route::get('/abort/500', function () {
+    abort(500);
+});
